e9017caf6225ab21bcc891a3cdeecfc331c191c1
Frédéric Thomann <96212318+FredericThomann@users.noreply.github.com>
2024-11-02 16:38:02 +0100

    Upload Files and Folders

---
+++ assets/scripts/script.js
@@ -0,0 +1,292 @@
+let inputBox = document.querySelectorAll('#step-1 .inputBox');
+let priceDetails = [0, 0, 0, 0];
+
+// ------------------------ SHOW STEPS & BUTTONS
+
+let progression = document.querySelectorAll('.step');
+let sidebar = document.querySelectorAll('.progress');
+let valid = true;
+let currentStep = 0;
+
+showStep(currentStep);
+
+function showStep(stepNb) {
+  progression[stepNb].style.display = 'flex';
+  sidebar[stepNb].classList.add('active');
+  document.getElementById('confirm').style.display = 'none';
+  if (stepNb === 0) {
+    document.getElementById('prev').style.visibility = 'hidden';
+  }
+  else {
+    document.getElementById('prev').style.visibility = 'visible';
+  }
+  if (stepNb === (progression.length - 1)) {
+    document.getElementById('next').style.display = 'none';
+    document.getElementById('confirm').style.display = 'block';
+  } 
+  else {
+    document.getElementById('next').style.display = 'block';
+  }
+}
+
+function validation() {
+  for (let i = 0; i < inputBox.length; i++) {
+    if (inputBox[i].querySelectorAll('input')[0].value === '') {
+      inputBox[i].querySelector('.invalid').classList.add('show');
+      let input = inputBox[i].querySelectorAll('input');
+      input[0].classList.add('invalid');
+      valid = false;
+    }
+  }
+  return valid;
+}
+
+function prevNext(stepNb) {
+  if(currentStep === 0 && !validation()) return false;
+  progression[currentStep].style.display = 'none';
+  sidebar[currentStep].classList.remove('active');
+  currentStep = currentStep + stepNb;
+  showStep(currentStep);
+}
+
+// ------------------------ STEP 1
+
+let inputs = document.querySelectorAll("#step-1 input");
+
+inputs.forEach((input, i) => {
+  input.addEventListener('blur', () => {
+    if(input.value !== '') {
+      inputBox[i].querySelector('.invalid').classList.remove('show');
+      input.classList.remove('invalid');
+    }
+    else {
+      inputBox[i].querySelector('.invalid').classList.add('show');
+      input.classList.add('invalid');
+    }
+  });
+});
+
+// ------------------------ STEP 2
+
+let durationSlider = document.querySelector('#durationSlider input');
+let checkPlans = document.getElementsByName("radioCard");
+let isMonthly = true;
+let checkedPlanNb = 0;
+priceDetails[0] = checkPrice(checkedPlanNb, null);
+
+document.querySelector('#monthlyOption').style.display = 'flex';
+document.querySelector('#monthDuration').style.color = 'var(--marineBlue)';
+checkPlans[0].checked = true;
+
+monthSelection();
+selectionName(checkedPlanNb);
+selectionOptions(checkedPlanNb);
+priceShow();
+
+durationSlider.addEventListener('change', (event) => {
+  if(event.target.checked) {
+    isMonthly = false;
+    yearlyChange();
+    yearSelection();
+    selectionName(checkedPlanNb);
+    selectionOptions(checkedPlanNb);
+    CheckOptions(checkedPlanNb);
+    priceShow();
+  }
+  else {
+    isMonthly = true;
+    monthlyChange();
+    monthSelection();
+    selectionName(checkedPlanNb);
+    selectionOptions(checkedPlanNb);
+    CheckOptions(checkedPlanNb);
+    priceShow();
+  }
+});
+
+checkPlans.forEach((selection, i) => {
+  selection.addEventListener('click', () => {
+    checkedPlanNb = i;
+    selectionName(checkedPlanNb);
+    selectionOptions(checkedPlanNb);
+    priceDetails[0] = checkPrice(checkedPlanNb, null);
+    priceShow();
+  });
+});
+
+function yearlyChange() {
+  document.querySelector('#monthlyOption').style.display = 'none';
+  document.querySelector('#monthDuration').style.color = 'var(--coolGray)';
+  document.querySelector('#yearlyOption').style.display = 'flex';
+  document.querySelector('#yearDuration').style.color = 'var(--marineBlue)';
+  checkPlans[3].checked = true;
+  checkedPlanNb = 3;
+  priceDetails[0] = checkPrice(checkedPlanNb, null);
+  priceShow();
+}
+
+function monthlyChange() {
+  document.querySelector('#yearlyOption').style.display = 'none';
+  document.querySelector('#yearDuration').style.color = 'var(--coolGray)';
+  document.querySelector('#monthlyOption').style.display = 'flex';
+  document.querySelector('#monthDuration').style.color = 'var(--marineBlue)';
+  checkPlans[0].checked = true;
+  checkedPlanNb = 0;
+  priceDetails[0] = checkPrice(checkedPlanNb, null);
+  priceShow();
+}
+
+// ------------------------ STEP 3
+
+let optionCase = [false, false, false];
+let options = document.querySelectorAll('.addonBox input');
+document.querySelector('.optionBox').style.display = 'none';
+
+function monthSelection() {
+  document.querySelector('#onlineService').innerHTML = '+&dollar;1/mo';
+  document.querySelector('#storage').innerHTML = '+&dollar;2/mo';
+  document.querySelector('#profile').innerHTML = '+&dollar;2/mo';
+}
+
+function yearSelection() {
+  document.querySelector('#onlineService').innerHTML = '+&dollar;10/yr';
+  document.querySelector('#storage').innerHTML = '+&dollar;20/yr';
+  document.querySelector('#profile').innerHTML = '+&dollar;20/yr';
+}
+
+options.forEach((option, i) => {
+  option.addEventListener('change', (event) => {
+    if(event.target.checked) {
+      optionCase[i] = true;
+      document.getElementById('option-' + i).style.display = 'flex';
+      priceDetails[i+1] = checkPrice(checkedPlanNb, i);
+      priceShow();
+    }
+    else {
+      optionCase[i] = false;
+      document.getElementById('option-' + i).style.display = 'none';
+      priceDetails[i+1] = 0;
+      priceShow();
+    }
+    optionCase.includes(true) ? document.querySelector('.optionBox').style.display = 'block' : document.querySelector('.optionBox').style.display = 'none';
+  });
+});
+
+// ------------------------ STEP 4
+
+function selectionName(option) {
+  switch (option) {
+    case 0:
+      document.getElementById('selection__name').innerHTML = 'Arcade (Monthly)';
+      document.getElementById('selection__price').innerHTML = '&dollar;9/mo';
+      break;
+    case 1:
+      document.getElementById('selection__name').innerHTML = 'Advanced (Monthly)';
+      document.getElementById('selection__price').innerHTML = '&dollar;12/mo';
+      break;
+    case 2:
+      document.getElementById('selection__name').innerHTML = 'Pro (Monthly)';
+      document.getElementById('selection__price').innerHTML = '&dollar;15/mo';
+      break;
+    case 3:
+      document.getElementById('selection__name').innerHTML = 'Arcade (Yearly)';
+      document.getElementById('selection__price').innerHTML = '&dollar;90/yr';
+      break;
+    case 4:
+      document.getElementById('selection__name').innerHTML = 'Advanced (Yearly)';
+      document.getElementById('selection__price').innerHTML = '&dollar;120/yr';
+      break;
+    case 5:
+      document.getElementById('selection__name').innerHTML = 'Pro (Yearly)';
+      document.getElementById('selection__price').innerHTML = '&dollar;150/yr';
+      break;
+    default:
+      break;
+  }
+}
+
+// ------------------------ CHECK PLANS & OPTIONS
+
+function CheckOptions(option) {
+  let options = document.querySelectorAll('.addonBox input');
+  options.forEach((checkedOption, i) => {
+    if(checkedOption.checked === true) {
+      priceDetails[i+1] = checkPrice(option, i);
+    }
+  });
+}
+
+function selectionOptions(option) {
+  switch (option) {
+    case 0:
+    case 1:
+    case 2:
+      document.querySelector('#option-0 .option__price').innerHTML = '+&dollar;1/mo';
+      document.querySelector('#option-1 .option__price').innerHTML = '+&dollar;2/mo';
+      document.querySelector('#option-2 .option__price').innerHTML = '+&dollar;2/mo';
+      break;
+    case 3:
+    case 4:
+    case 5:
+      document.querySelector('#option-0 .option__price').innerHTML = '+&dollar;10/yr';
+      document.querySelector('#option-1 .option__price').innerHTML = '+&dollar;20/yr';
+      document.querySelector('#option-2 .option__price').innerHTML = '+&dollar;20/yr';
+      break;
+    default:
+      break;
+  }
+}
+
+function checkPrice(plan, option) {
+  if(option === null) {
+    switch (plan) {
+      case 0:
+        return 9;
+      case 1:
+        return 12;
+      case 2:
+        return 15;
+      case 3:
+        return 90;
+      case 4:
+        return 120;
+      case 5:
+        return 150;
+      default:
+        break;
+    }
+  }
+  if(option === 0 && plan < 3) {
+    return 1;
+  }
+  if(option === 0 && plan >= 3) {
+    return 10;
+  }
+  if((option === 1 || option === 2) && plan < 3) {
+    return 2;
+  }
+  if((option === 1 || option === 2) && plan >= 3) {
+    return 20;
+  }
+}
+
+function priceShow() {
+  let x = 0;
+  priceDetails.forEach(price => {
+    x += price;
+  });
+  if(isMonthly) {
+    document.querySelector('.totalPrice__price').innerHTML = `&dollar;${x}/mo`;
+  }
+  else {
+    document.querySelector('.totalPrice__price').innerHTML = `&dollar;${x}/yr`;
+  }
+}
+
+// ------------------------ CONFIRMATION
+
+document.getElementById('multistepForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+  document.querySelector('#multistepForm').style.display = 'none';
+  document.querySelector('#confirmation').style.display = 'flex';
+});
